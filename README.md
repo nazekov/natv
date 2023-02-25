ab20737c6c0395d019712f975a98acaeea4309fe
nazekov <nazekov.ul@gmail.com>
2023-02-25 20:28:11 +0600

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# natv final training project
+
+* This project was implemented on the basis of the site https://natv.kg 
---
+++ src/main/java/kg/mega/natv/NatvApplication.java
@@ -0,0 +1,12 @@
+package kg.mega.natv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatvApplication.class, args);
+	}
+}
