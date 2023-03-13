a281e96d1c5bd63eaaa5e984a6cd98e9e92ff593
nazekov <nazekov.ul@gmail.com>
2023-03-13 21:52:05 +0600

    renamed DiscountTextRepository to DiscountRepository. added method findDiscounts

--- src/main/java/kg/mega/natv/repository/DiscountRepository.java
+++ src/main/java/kg/mega/natv/repository/DiscountRepository.java
@@ -1,15 +1,16 @@
 package kg.mega.natv.repository;
 
-import kg.mega.natv.model.entity.DiscountText;
+import kg.mega.natv.model.dto.IDiscountDto;
+import kg.mega.natv.model.entity.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface DiscountTextRepository extends JpaRepository<DiscountText, Long> {
+public interface DiscountRepository extends JpaRepository<Discount, Long> {
 
     @Query(value = "select discount from " +
                     "(select from_days_count, discount_percent as discount " +
@@ -20,4 +21,11 @@ public interface DiscountTextRepository extends JpaRepository<DiscountText, Long
                     "limit 1",
             nativeQuery = true)
     Optional<Double> findDiscount(Long channelId, Date date, Integer countDays);
+
+    @Query(value = "select from_days_count as fromDaysCount, " +
+                            "discount_percent as discountPercent " +
+                    "from tb_discounts_text " +
+                    "where channel_id = ?1 and end_date = '2999-12-31 00:00:00'",
+            nativeQuery = true)
+    List<IDiscountDto> findDiscounts(Long channelId);
 }
