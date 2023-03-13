2f8ffcfc6c3c0976acabb762b00aa15ae1dce187
nazekov <nazekov.ul@gmail.com>
2023-03-13 22:00:13 +0600

    created OrderException

---
+++ src/main/java/kg/mega/natv/exception_handle/exception/OrderException.java
@@ -0,0 +1,8 @@
+package kg.mega.natv.exception_handle.exception;
+
+public class OrderException extends RuntimeException {
+
+    public OrderException(String message) {
+        super(message);
+    }
+}
