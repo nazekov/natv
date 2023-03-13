b397604701ff6ec9b200b2fd816e1add5ff017e1
nazekov <nazekov.ul@gmail.com>
2023-03-13 21:45:57 +0600

    added to ChannelRepository methods findDistinctByPathLogoEndingWith, findAllByActive

--- src/main/java/kg/mega/natv/repository/ChannelRepository.java
+++ src/main/java/kg/mega/natv/repository/ChannelRepository.java
@@ -2,7 +2,9 @@ package kg.mega.natv.repository;
 
 import kg.mega.natv.model.entity.Channel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -10,7 +12,14 @@ public interface ChannelRepository extends JpaRepository<Channel, Long> {
 
     Optional<Channel> findByNameIgnoreCase(String name);
 
-//    Optional<Channel> findById(Long id);
-
     boolean existsById(Long id);
+
+    Optional<String> findDistinctByPathLogoEndingWith(String logoName);
+
+    @Query(value = "select * from tb_channels " +
+                    "where active = ?1 " +
+                    "order by name ",
+            nativeQuery = true)
+    List<Channel> findAllByActive(boolean active);
+
 }
