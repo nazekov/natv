0202f6cd13d50bb28f0748cecfdb016d41a072fa
nazekov <nazekov.ul@gmail.com>
2023-03-13 21:53:50 +0600

    created OrderRepository

---
+++ src/main/java/kg/mega/natv/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package kg.mega.natv.repository;
+
+import kg.mega.natv.model.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
