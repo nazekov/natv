e00512ea5a2bffde22b45b55f1e13c6bf203e96e
nazekov <nazekov.ul@gmail.com>
2023-03-13 21:54:51 +0600

    renamed PriceTextRepository to PriceRepository.

--- src/main/java/kg/mega/natv/repository/PriceRepository.java
+++ src/main/java/kg/mega/natv/repository/PriceRepository.java
@@ -1,14 +1,13 @@
 package kg.mega.natv.repository;
 
-import kg.mega.natv.model.entity.PriceText;
+import kg.mega.natv.model.entity.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.Date;
 
 @Repository
-public interface PriceTextRepository extends JpaRepository<PriceText, Long> {
+public interface PriceRepository extends JpaRepository<Price, Long> {
 
     @Query(value = "select price_per_letter from tb_prices_text " +
                     "where channel_id = ?1 and end_date = ?2",
