06eb985f4620345d0e999483d472b782ea165616
nazekov <nazekov.ul@gmail.com>
2023-03-14 12:43:50 +0600

    deleted method dtoToDiscountEntityList and instead of its created idtosToDiscountDtoList, dtoToDiscountEntityList

--- src/main/java/kg/mega/natv/mapper/DiscountMapper.java
+++ src/main/java/kg/mega/natv/mapper/DiscountMapper.java
@@ -1,11 +1,9 @@
 package kg.mega.natv.mapper;
 
-import kg.mega.natv.model.dto.ChannelDto;
 import kg.mega.natv.model.dto.DiscountDto;
-import kg.mega.natv.model.entity.Channel;
-import kg.mega.natv.model.entity.DiscountText;
+import kg.mega.natv.model.dto.IDiscountDto;
+import kg.mega.natv.model.entity.Discount;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import java.util.List;
 
@@ -14,8 +12,7 @@ public interface DiscountMapper {
 
     DiscountMapper INSTANCE = Mappers.getMapper(DiscountMapper.class);
 
+    List<Discount> dtoToDiscountEntityList(List<DiscountDto> discounts);
 
-    DiscountText dtoToDiscountEntity(DiscountDto discount);
-
-    List<DiscountText> dtoToDiscountEntityList(List<DiscountDto> discounts);
+    List<DiscountDto> idtosToDiscountDtoList(List<IDiscountDto> iDiscountDtos);
 }
