4907ee18dd3cae47e00347e6430760d00ec7543f
nazekov <nazekov.ul@gmail.com>
2023-03-14 20:23:11 +0600

    added checking of new price

--- src/main/java/kg/mega/natv/controller/PriceController.java
+++ src/main/java/kg/mega/natv/controller/PriceController.java
@@ -1,10 +1,14 @@
 package kg.mega.natv.controller;
 
+import kg.mega.natv.exception_handle.exception.InputInfoChannelException;
 import kg.mega.natv.model.dto.request_dto.PriceRequestDto;
 import kg.mega.natv.model.entity.Channel;
 import kg.mega.natv.service.PriceService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/price")
@@ -19,6 +23,9 @@ public class PriceController {
     @PostMapping("/save")
     public ResponseEntity<Channel> addNewPriceToChannel(
             @RequestBody PriceRequestDto priceRequestDto) {
+        if (priceRequestDto.getPricePerLetter() <= 0) {
+            throw new InputInfoChannelException("price must be positive number.");
+        }
         return ResponseEntity.ok(priceService.addPrice(priceRequestDto));
     }
 }
