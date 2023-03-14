aa7f0449b6a62ff83ef6437e99f3ed4d959ad29d
nazekov <nazekov.ul@gmail.com>
2023-03-14 12:53:52 +0600

    Changed parameters in method declarations and added one method

--- src/main/java/kg/mega/natv/service/ValidateService.java
+++ src/main/java/kg/mega/natv/service/ValidateService.java
@@ -1,12 +1,15 @@
 package kg.mega.natv.service;
 
-import kg.mega.natv.model.dto.ChannelDto;
-import kg.mega.natv.model.dto.TextOrderDto;
+import kg.mega.natv.model.dto.request_dto.ChannelCreateDto;
+import kg.mega.natv.model.dto.request_dto.OrderRequestDto;
+import kg.mega.natv.model.dto.request_dto.TextOrderRequestDto;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ValidateService {
 
-    void checkInputData(MultipartFile file, ChannelDto channelDto);
+    void checkInputData(MultipartFile file, ChannelCreateDto channelDto);
 
-    void checkInputData(TextOrderDto textOrderDto);
+    void checkInputData(TextOrderRequestDto textOrderRequestDto);
+
+    void checkInputData(OrderRequestDto orderDto);
 }
