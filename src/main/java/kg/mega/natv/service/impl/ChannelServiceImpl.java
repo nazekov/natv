0eab4d3b80d8dc74be0ed87e9aa9bf519d0e4d24
nazekov <nazekov.ul@gmail.com>
2023-03-14 12:51:30 +0600

    Deleted @Transactional over class and added it to save(). Created dependency priceService, discountService. Also changed save(). Added implementaions methods of ChannelService interface:  findById, existsByLogoName, showAllChannels, addNewPrice

--- src/main/java/kg/mega/natv/service/impl/ChannelServiceImpl.java
+++ src/main/java/kg/mega/natv/service/impl/ChannelServiceImpl.java
@@ -1,16 +1,21 @@
 package kg.mega.natv.service.impl;
 
 import kg.mega.natv.exception_handle.exception.ImageException;
+import kg.mega.natv.exception_handle.exception.InputInfoChannelException;
+import kg.mega.natv.mapper.ChannelMapper;
 import kg.mega.natv.mapper.DiscountMapper;
-import kg.mega.natv.model.dto.ChannelDto;
+import kg.mega.natv.model.dto.request_dto.PriceRequestDto;
+import kg.mega.natv.model.dto.response_dto.ChannelResponseDto;
+import kg.mega.natv.model.dto.request_dto.ChannelCreateDto;
 import kg.mega.natv.model.entity.Channel;
-import kg.mega.natv.model.entity.DiscountText;
-import kg.mega.natv.model.entity.PriceText;
+import kg.mega.natv.model.entity.Discount;
+import kg.mega.natv.model.entity.Price;
 import kg.mega.natv.repository.ChannelRepository;
 import kg.mega.natv.service.ChannelService;
+import kg.mega.natv.service.DiscountService;
+import kg.mega.natv.service.PriceService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.transaction.Transactional;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
@@ -19,46 +24,42 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-
 import static java.nio.file.StandardOpenOption.APPEND;
 import static java.nio.file.StandardOpenOption.CREATE;
 
 @Service
-@Transactional
 public class ChannelServiceImpl implements ChannelService {
 
     private final ChannelRepository channelRepository;
 
-    public ChannelServiceImpl(ChannelRepository channelRepository) {
+    private final PriceService priceService;
+
+    private final DiscountService discountService;
+
+    public ChannelServiceImpl(ChannelRepository channelRepository,
+                              PriceService priceService,
+                              DiscountService discountService) {
         this.channelRepository = channelRepository;
+        this.priceService = priceService;
+        this.discountService = discountService;
     }
 
+    @Transactional
     @Override
-    public Channel save(MultipartFile file, ChannelDto channelDto) {
+    public Channel save(MultipartFile file, ChannelCreateDto channelDto) {
         Channel channel = new Channel();
         channel.setName(channelDto.getName());
         channel.setPathLogo(saveLogo(file));
-
-        PriceText priceText = new PriceText();
-        priceText.setPricePerLetter(channelDto.getPricePerLetter());
-
-        List<DiscountText> discountTexts =
-                DiscountMapper.INSTANCE.dtoToDiscountEntityList(channelDto.getDiscounts());
-
-        channel.addNewPriceText(priceText);
-//        List<PriceText> priceTexts = Arrays.asList(priceText);
-
-//        channel.setPricesText(priceTexts);
-        channel.setDiscountsText(discountTexts);
-
-        channelRepository.save(channel);
-
-//        priceTexts.stream()
-//                .forEach(priceText1 -> priceText1.setChannel(channel));
-        discountTexts.stream()
-                .forEach(discountText -> discountText.setChannel(channel));
-
-        return channel;
+        Price price = new Price();
+        price.setPricePerLetter(channelDto.getPricePerLetter());
+        List<Discount> discounts = DiscountMapper.INSTANCE
+                        .dtoToDiscountEntityList(channelDto.getDiscounts());
+        channel.addNewPriceText(price);
+        channel.setDiscountsText(discounts);
+        if (discounts != null) {
+            discounts.forEach(discount -> discount.setChannel(channel));
+        }
+        return channelRepository.save(channel);
     }
 
     private String saveLogo(MultipartFile file) {
@@ -83,5 +84,52 @@ public class ChannelServiceImpl implements ChannelService {
     @Override
     public boolean existsById(Long id) {
         return channelRepository.existsById(id);
+    }
+
+    @Override
+    public Channel findById(Long id) {
+        return channelRepository.findById(id).get();
+    }
+
+    @Override
+    public boolean existsByLogoName(String logoName) {
+        return channelRepository
+                .findDistinctByPathLogoEndingWith(logoName)
+                .isPresent();
+    }
+
+    @Override
+    public List<ChannelResponseDto> showAllChannels() {
+        List<Channel> channelList = channelRepository.findAllByActive(true);
+
+        List<ChannelResponseDto> channelDtos
+                = ChannelMapper.INSTANCE.channelListToListDto(channelList);
+
+        channelDtos.forEach(channelDto -> {
+            channelDto.setPricePerLetter(
+                    priceService
+                        .findPriceByChannelIdAndEndDate(channelDto.getId())
+                );
+            channelDto.setDiscounts(
+                    DiscountMapper.INSTANCE
+                        .idtosToDiscountDtoList(
+                            discountService.findDiscounts(channelDto.getId())
+                        )
+                );
+            }
+        );
+
+        return channelDtos;
+    }
+
+    @Override
+    public Channel addNewPrice(PriceRequestDto priceRequestDto) {
+        Long channelId = priceRequestDto.getChannelId();
+        Channel channel = channelRepository.findById(channelId)
+                .orElseThrow(() -> new InputInfoChannelException("Channel not found"));
+        Price price = new Price();
+        price.setPricePerLetter(priceRequestDto.getPricePerLetter());
+        channel.addNewPriceText(price);
+        return channel;
     }
 }
