bb62c6aa0086628de2e51ecb568f49e7bd176816
nazekov <nazekov.ul@gmail.com>
2023-03-14 20:41:56 +0600

    changed query and method for findDiscount

--- src/main/java/kg/mega/natv/repository/DiscountRepository.java
+++ src/main/java/kg/mega/natv/repository/DiscountRepository.java
@@ -5,7 +5,6 @@ import kg.mega.natv.model.entity.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,17 +14,17 @@ public interface DiscountRepository extends JpaRepository<Discount, Long> {
     @Query(value = "select discount from " +
                     "(select from_days_count, discount_percent as discount " +
                     "from tb_discounts " +
-                    "where channel_id = ?1 and end_date = ?2 " +
+                    "where channel_id = ?1 and end_date = '2999-12-31 00:00:00' " +
                     "order by from_days_count desc) as sub_tb " +
-                    "where ?3 >= from_days_count " +
+                    "where ?2 >= from_days_count " +
                     "limit 1",
             nativeQuery = true)
-    Optional<Double> findDiscount(Long channelId, Date date, Integer countDays);
+    Optional<Double> findDiscount(Long channelId, Integer countDays);
 
     @Query(value = "select from_days_count as fromDaysCount, " +
                             "discount_percent as discountPercent " +
                     "from tb_discounts " +
-                    "where channel_id = ?1 and end_date = '2999-12-31 00:00:00'",
+                    "where channel_id = ?1 and end_date = '2999-12-31 00:00:00' ",
             nativeQuery = true)
     List<IDiscountDto> findDiscounts(Long channelId);
 }
