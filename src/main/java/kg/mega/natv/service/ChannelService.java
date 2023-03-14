9d9490575125b21dcb51e1a00156048081168a8c
nazekov <nazekov.ul@gmail.com>
2023-03-14 12:46:51 +0600

    renamed parameter in save() and created findById(), existsByLogoName(), showAllChannels(), addNewPrice()

--- src/main/java/kg/mega/natv/service/ChannelService.java
+++ src/main/java/kg/mega/natv/service/ChannelService.java
@@ -1,14 +1,25 @@
 package kg.mega.natv.service;
 
-import kg.mega.natv.model.dto.ChannelDto;
+import kg.mega.natv.model.dto.request_dto.PriceRequestDto;
+import kg.mega.natv.model.dto.response_dto.ChannelResponseDto;
+import kg.mega.natv.model.dto.request_dto.ChannelCreateDto;
 import kg.mega.natv.model.entity.Channel;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
 
 public interface ChannelService {
 
-    Channel save(MultipartFile file, ChannelDto channelDto);
+    Channel save(MultipartFile file, ChannelCreateDto channelDto);
 
     boolean existsByName(String name);
 
     boolean existsById(Long id);
+
+    Channel findById(Long id);
+
+    boolean existsByLogoName(String logoName);
+
+    List<ChannelResponseDto> showAllChannels();
+
+    Channel addNewPrice(PriceRequestDto priceRequestDto);
 }
